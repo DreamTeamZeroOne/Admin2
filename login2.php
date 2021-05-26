a6d180f9a88032f923ea8b9e340043c078a66dee
ajcotoner <78724958+ajcotoner@users.noreply.github.com>
2021-05-26 12:18:28 +0800

    login page
    may binago sa login page design

---
+++ login2.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="login-page">
+    
+
+    <div class="login-form">
+        
+   
+    <div class="login-container">
+
+        <div class="login-main">
+            <div class="form-img">
+        <img src="./images/log.png" alt="">
+
+    </div>
+            <div class="login-content">
+                <h2>Log In</h2>
+                <form action="" method="post">
+                    <input type="text" name="" placeholder="Username" required autofocus="">
+                    <input type="text" name="" placeholder="Username" required autofocus="">
+                    <button class="btn" type="submit">Login</button>
+                </form>
+                    <p class="account">Forgot your password?<a href="#">Forgot Password</a></p>
+    
+            </div>
+            
+        </div>
+    
+    </div>
+    
+    </div>
+
+    </div>
+
+</body>
+</html>
